edcd77901a62ad2fae56cae470a53d65a60eb002
IgweEmmanuel <chigozieigweemmanuel@gmail.com>
2024-03-27 15:58:03 +0000

    binary tree height

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -11,14 +11,14 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	size_t left;
 	size_t right;
 
-	if ((tree == NULL) || ((tree->left == NULL) && (tree->right ==NULL)))
+	if ((tree == NULL) || ((tree->left == NULL) && (tree->right == NULL)))
 		return (0);
 
-	left = binary_tree_height(tree->left); 
+	left = binary_tree_height(tree->left);
 	right = binary_tree_height(tree->right);
 
 	if (left >= right)
-		return (left) + 1;
+		return (left + 1);
 	else
-		return (right) + 1;
+		return (right + 1);
 }
