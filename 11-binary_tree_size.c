57a7c0a6b0a0fc032f6bf558e111459a65af268f
IgweEmmanuel <chigozieigweemmanuel@gmail.com>
2024-03-27 17:04:42 +0000

    size of tree

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -2,14 +2,14 @@
 #include <stddef.h>
 
 /**
- * binary_tree_size - this outputs the tree size 
+ * binary_tree_size - this outputs the tree size
  * @tree: this is the node
  *
  * Return: size_t
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
- 	size_t size_of_tree;
+	size_t size_of_tree;
 	size_t left;
 	size_t right;
 
