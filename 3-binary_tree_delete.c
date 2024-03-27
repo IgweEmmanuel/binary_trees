878b47a69019288a91adab42df1d8d55f208f8ef
IgweEmmanuel <chigozieigweemmanuel@gmail.com>
2024-03-27 12:30:51 +0000

    binary tree node

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_delete - this is the functio to delete node
+ * @tree: this is the node to delete
+ *
+ * Return: always returns 0 success
+ */
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return;
+	binary_tree_delete(tree->left);
+	binary_tree_delete(tree->right);
+
+	free(tree)
+}
