60207b80a0ef9578939f31758840287a63efe056
IgweEmmanuel <chigozieigweemmanuel@gmail.com>
2024-03-27 22:00:56 +0000

    insert right

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -13,12 +13,12 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	binary_tree_t *new_node;
 
 	if (parent == NULL)
-		return NULL;
+		return (NULL);
 
 	new_node = malloc(sizeof(binary_tree_t));
 
 	if (new_node == NULL)
-		return NULL;
+		return (NULL);
 
 	new_node->parent = parent;
 	new_node->n = value;
@@ -33,5 +33,5 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 
 	parent->right = new_node;
 
-	return new_node;
+	return (new_node);
 }
